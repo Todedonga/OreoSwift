53b0cea015fb06ff1f20c73156bf6614ea9ca6ac
Todedonga <guillermo.flores@iest.edu.mx>
2025-09-19 17:56:54 -0600

    Initial Commit

---
+++ OreoTests/OreoTests.swift
@@ -0,0 +1,17 @@
+//
+//  OreoTests.swift
+//  OreoTests
+//
+//  Created by win603 on 19/09/25.
+//
+
+import Testing
+@testable import Oreo
+
+struct OreoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
