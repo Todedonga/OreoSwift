922d59fc9ea177f88d907a93679ec2c10d306c21
Todedonga <guillermo.flores@iest.edu.mx>
2025-09-19 18:34:45 -0600

    cambios recientes

---
+++ Oreo/TrainCardView.swift
@@ -0,0 +1,18 @@
+//
+//  TrainCardView.swift
+//  Oreo
+//
+//  Created by win603 on 19/09/25.
+//
+
+import SwiftUI
+
+struct TrainCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrainCardView()
+}
